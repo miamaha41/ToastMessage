600a9481466591087573d8407f2a081f6757fd28
Nguyễn Thị Hải Yến <you@example.com>
2021-10-20 14:47:36 +0700

    change js mobile ux

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,8 @@ function toast({
     duration = 3000
 }) {
     const main = document.getElementById('toast');
-    if (main) {
+
+    function addToast() {
         const toast = document.createElement('div');
         //Auto remove Toast
         const autoRemoveId = setTimeout(() => main.removeChild(toast), duration + 1000);
@@ -30,14 +31,20 @@ function toast({
         toast.classList.add('toast', `toast--${type}`);
         toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
         toast.innerHTML = `<div class="toast__icon "><i class="${icon}"></i></div>
-            <div class="toast__body">
-                <h3 class="toast__title">${title}</h3>
-                <p class="toast__msg">${message}</p>
-            </div>
-            <div class="toast__close"><i class="fas fa-times"></i></div>`;
+        <div class="toast__body">
+            <h3 class="toast__title">${title}</h3>
+            <p class="toast__msg">${message}</p>
+        </div>
+        <div class="toast__close"><i class="fas fa-times"></i></div>`;
         main.appendChild(toast);
-
     }
+    if (!main.children.length) {
+        addToast();
+    } else if (main.children.length) {
+        main.removeChild(main.children[0]);
+        addToast();
+    }
+
 }
 
 function showSuccessToast() {
